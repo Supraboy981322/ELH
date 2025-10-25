7827ceb1ef2411b0aaa4fa49eec715106167c5d2
Zane <97927259+Supraboy981322@users.noreply.github.com>
2025-10-25 15:23:35 -0500

    not important

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
-//	"os"
+	"path/filepath"
+	"os"
+	"errors"
 )
 
 const (
 	port = "8080"
+)
+
+var (
+	suppNoExt = []string{
+		".html",
+		".egp",
+	}
 )
 
 func main() {
@@ -17,7 +26,48 @@ func main() {
 }
 
 func httpHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("\nreq:  %s\n", r.URL.Path)
-	
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Printf("GET:  %s\n", r.URL.Path)
+		getHandler(w, r)
+	default:
+		fmt.Printf("attempted bad method:  %s\n", r.Method)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Path
+	if file ==  "/" {
+		file = "index"
+	}
+	ext := filepath.Ext(file)
+	fmt.Println(ext)
+	if ext == "" {
+		for i := 0; i < len(suppNoExt); i++ {
+			checkFile := fmt.Sprintf("%s%s", file, suppNoExt[i])
+			_, err := os.Stat(checkFile)
+			if err == nil {
+				file = checkFile
+				ext = suppNoExt[i]
+				break
+			} else if !errors.Is(err, os.ErrNotExist) {
+				errOut("cannot check if file exists! Schrodinger's file", err)
+			}
+		}
+	}
+
+	if ext == ".egp" {
+		
+	}
+
+	fmt.Printf("\nreq:  %s\n", file)
+
 	fmt.Fprintf(w, "foo")
+}
+
+func errOut(str string, err error) {
+	errStr := fmt.Sprintf("\n\n%s:\n%v\n", str, err)
+	errVal := errors.New(errStr)
+	panic(errVal)
 }
