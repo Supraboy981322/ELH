b2839b66fbe1bfdc57a3db6218dde1e13b66a66e
zane <97927259+Supraboy981322@users.noreply.github.com>
2025-11-27 17:44:36 -0600

    started work on libs

--- headers.go
+++ headers.go
@@ -1,5 +1,72 @@
 package elh
 
-const (
-	vimHead = ""
+var (
+	libs = map[string]string{
+		//python libs
+		"py":    ``,
+
+		//js libs
+		"js":    ``,
+
+		//bash libs
+		"bash":  `#!/usr/bin/env bash
+
+headers='{"foo":"bar"}'
+
+headers() {
+  case "$1" in
+    "get")
+      if [[ "$2" == "" ]]; then
+        printf "no header provided"
+      fi
+      ;;
+    *)
+      printf "$headers" | jq
+      ;;
+  esac
+}
+
+linesToHTML() {
+  local stdin=$(< /dev/stdin) 
+  printf "${stdin}\n" \
+    | sed "s|^|<${1:-p}>|g" \
+    | sed "s|$|</${1:-p}>|g"
+}
+
+linkFiles() {
+  for file in $(ls); do
+    printf "<a href=\"${file}\">${file}</a>\n"
+  done
+}`,
+
+		//java libs
+		"java":  ``,
+		
+		//lua libs
+		"lua":   ``,
+
+		//go libs
+		"go":    ``,
+
+		//brainfuck libs
+		"bf":    ``,
+
+		//r libs
+		"r":     ``,
+
+		//ruby libs
+		"ruby":  ``,
+
+		//php libs
+		"php":   ``,
+
+		//perl libs
+		"perl":  ``,
+
+		//basic libs
+		"basic": ``,
+
+		//vim libs
+		"vim":   ``,
+	}
 )
